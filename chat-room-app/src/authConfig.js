2fbc160eab2cff65cdfbadf88f3a7576cbca47ea
jhonny <giovanni.forlenza.gf@gmail.com>
2025-06-03 08:38:59 +0000

    debugging

--- chat-room-app/src/authConfig.js
+++ chat-room-app/src/authConfig.js
@@ -1,3 +1,7 @@
+console.log('CLIENT_ID:', process.env.REACT_APP_CLIENT_ID);
+console.log('TENANT_ID:', process.env.REACT_APP_TENANT_ID);
+console.log('All process.env:', Object.keys(process.env).filter(key => key.startsWith('REACT_APP')));
+
 export const msalConfig = {
     auth: {
         clientId: process.env.REACT_APP_CLIENT_ID || 'MISSING_CLIENT_ID',
