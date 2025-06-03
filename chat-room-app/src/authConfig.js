2970bb1a24474ad0a3b04b24264b4b46edcc1df4
jhonny <giovanni.forlenza.gf@gmail.com>
2025-06-03 09:39:12 +0000

    Update authority

--- chat-room-app/src/authConfig.js
+++ chat-room-app/src/authConfig.js
@@ -1,7 +1,7 @@
 export const msalConfig = {
     auth: {
         clientId: process.env.REACT_APP_CLIENT_ID || 'MISSING_CLIENT_ID',
-        authority: `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID}` || 'MISSING_TENANT_ID',
+        authority: `https://login.microsoftonline.com/organizations` || 'MISSING_TENANT_ID',
         redirectUri: window.location.origin
     },
     cache: {
